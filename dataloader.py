6f7bb5e8498742e901132ac1688fc7e0f002df95
zblednu <maksym.surovtsev@student.tuke.sk>
2025-04-15 07:19:21 +0200

    update logic for processing and loading

--- dataloader.py
+++ dataloader.py
@@ -4,10 +4,28 @@ import torch
 from torch.utils.data import Dataset
 
 class RyersonEmotionDataset(Dataset):
-    def __init__(self, dataset_dir):
+    def __init__(self, path):
+        label_map = {
+            'happy': 0,
+            'sad': 1,
+            'angry': 2,
+            'fear': 3,
+            'surpise': 4,
+            'disgust': 5,
+        }
+        self.images = [];
+        for root, _, files in os.walk(path):
+        for file in files:
+            if file.endswith('.avi'):
+                vid_paths.append(os.path.join(root, file))
 
     def __len__(self):
         return len(self.images)
 
     def __getitem__(self, idx):
+        image = self.images[idx]
+        #some cv2 function to read the image
+        #take last part to get label
+        face_normalized = face_resized.astype('float32') / 255.0
+        image_as_tensor = torch.unsqueeze(torch.tensor(face_normalized), 0) 
         return (self.images[idx], self.labels[idx])
--- preprocess.py
+++ preprocess.py
@@ -1,7 +1,11 @@
 import os
 import cv2
 
-def vids_to_frames(path)
+def vids_to_frames(path, output_dir)
+    frame_count = 0
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+
     vid_paths = []
     for root, _, files in os.walk(path):
     for file in files:
@@ -9,34 +13,32 @@ def vids_to_frames(path)
             vid_paths.append(os.path.join(root, file))
         
     label_map = {
-        'ha': 0,
-        'sa': 1,
-        'an': 2,
-        'fe': 3,
-        'su': 4,
-        'di': 5,
+        'ha': 'happy',
+        'sa': 'sad',
+        'an': 'angry',
+        'fe': 'fear',
+        'su': 'surpise',
+        'di': 'disgust',
     }
 
     face_finder = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
-    for vid in vid_paths[0:50]:
-    label = label_map[os.path.basename(vid)[0:2]]
+    for vid in vid_paths:
+        label = label_map[os.path.basename(vid)[0:2]]
 
-    video = cv2.VideoCapture(vid)
+        video = cv2.VideoCapture(vid)
 
-    while True:
-        ret, frame = video.read()
-        if not ret:
-            break
+        while True:
+            ret, frame = video.read()
+            if not ret:
+                break
 
-        gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        for x, y, w, h in face_finder.detectMultiScale(gray_frame, 1.1, 4):
-            face = gray_frame[y:y+h, x:x+w]
-            face_resized = cv2.resize(face, (64, 64))
-            face_normalized = face_resized.astype('float32') / 255.0
-            image_as_tensor = torch.unsqueeze(torch.tensor(face_normalized), 0) 
+            gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+            for (x, y, w, h) in face_finder.detectMultiScale(gray_frame, 1.1, 4): # how the fuck does this function work??
+                face = gray_frame[y:y+h, x:x+w]
+                face_resized = cv2.resize(face, (64, 64))
 
-            print(image_as_tensor.shape)
-            self.images.append(image_as_tensor)
-            self.labels.append(label)
+                output_path = os.path.join(output_dir, f'{frame_count}{label}.jpg')
+                cv2.imwrite(output_path, face_resized)
+                frame_count += 1
 
-    video.release()
+        video.release()
